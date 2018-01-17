5cc4e341586096f550dec073643833055090ce83
Inie <me@email.com>
2018-01-16 18:06:33 -0600

    Validation created

--- guess_the_number.py
+++ guess_the_number.py
@@ -17,20 +17,20 @@ def generate_secret(low, high):
 
 
 def get_guess():
-    '''get user's guess'''if num <1 or num > 10:
-
-        num = int(input('Guess the secret number? '))
-        print('Enter a valid number')
-    return int(input('Guess the secret number? '))
+    '''get user's guess'''
+    num = int(input('Guess the secret number? '))
+    while (num <1 or num >10):
+        num = int(input('The number is out of range. Guess the secret number again? '))
+    return num
 
 
 def check_guess(guess, secret):
     '''compare guess and secret, return string describing result of comparison'''
     if guess == secret:
         return correct
-    if guess < secret:
+    if (guess < secret):
         return too_low
-    if guess > secret:
+    if (guess > secret):
         return too_high
 
 
